c2e23851e6b957eddde7f7eab5dbf7b584b55c64
Kevin Tang <just.ktang@gmail.com>
2020-07-19 15:06:06 -0400

    added if and ask

--- src/interpreter/flattener.h
+++ src/interpreter/flattener.h
@@ -4,10 +4,20 @@
 
 #include "abstract.h"
 
+struct JumpContext
+{
+    std::vector<extArg_t> UnresolvedJumps;
+    std::vector<extArg_t> UnresolvedJumpFalses;
+};
+
 void FlattenProgram(Program* p);
 void FlattenBlock(Block* block);
 void FlattenOperation(Operation* op);
 
 int NOPSafetyDomainSize();
+
+void FlattenOperationScopeResolution(Operation* op);
+void FlattenOperationScopeResolutionWithDereference(Operation* op);
+
 
 #endif
