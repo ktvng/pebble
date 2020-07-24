d2d4635f7178acec83161cb35eabef9809aeb2a1
henesy <henesy.dev@gmail.com>
2020-07-23 17:02:01 -0700

    s/textbuild/testbuild/ ;; fix pebble_testbuild.exe target by updating assets/test_main.cpp to be in line with new commandargs.h interfaces ;; add Usage() to assets/test_main.cpp

--- assets/test_main.cpp
+++ assets/test_main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <string>
+#include <ctime>
+#include <chrono>
+#include <ratio>
 
 #include "main.h"
 #include "program.h"
@@ -8,25 +10,42 @@
 #include "operation.h"
 #include "reference.h"
 #include "parse.h"
+#include "execute.h"
 #include "commandargs.h"
+#include "vm.h"
+#include "flattener.h"
+#include "scope.h"
 
-void SettingIgnoreCustom(std::vector<SettingOption> options)
+bool Usage(std::vector<SettingOption> options)
+{
+    std::cerr << "Usage pebble: [--help] [--ignore-custom] [--noisy] [--bytecode] [program.pebl]" << std::endl;
+
+    exit(2);
+}
+
+bool SettingIgnoreCustom(std::vector<SettingOption> options)
 {
     g_shouldRunCustomProgram = false;
+    return false;
 }
 
-void SettingNoisy(std::vector<SettingOption> options)
+bool SettingNoisy(std::vector<SettingOption> options)
 {
     g_noisyReport = true;
+    return false;
 }
 
-void SettingUseBytecodeRuntime(std::vector<SettingOption> options)
+bool SettingUseBytecodeRuntime(std::vector<SettingOption> options)
 {
     g_useBytecodeRuntime = true;
+    return false;
 }
 
 ProgramConfiguration Config
 {
+    {
+        "Pebl File", "program.pebl", nullptr
+    },
     { 
         "Ignore ./program.pebl", "--ignore-custom", SettingIgnoreCustom
     },
@@ -45,7 +64,7 @@ bool g_useBytecodeRuntime = false;
 
 int main(int argc, char *argv[])
 {
-    ParseCommandArgs(argc, argv, Config);
+    ParseCommandArgs(argc, argv, &Config);
     
     bool testsFailed = Test();
     if(testsFailed)
