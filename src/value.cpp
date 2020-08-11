33ed66eb27be613a40ba873a39875ae8a53ab7b3
Kevin Tang <just.ktang@gmail.com>
2020-08-11 18:24:21 -0400

    added comments

--- src/value.cpp
+++ src/value.cpp
@@ -2,12 +2,13 @@
 
 #include "value.h"
 
-
+/// wrapper which should be used to destroy a std::string used as a Call value
 void StringDestructor(String* s)
 {
     delete s;
 }
 
+/// wrapper to construct a new std::string used as a Call value
 String* StringConstructor(const String& value)
 {
     std::string* s = new std::string;
@@ -15,21 +16,25 @@ String* StringConstructor(const String& value)
     return s;
 }
 
+/// wrapper to assign [v] to [value]
 void AssignValue(Value& v, const String& value)
 {
     v.s = StringConstructor(value);
 }
 
+/// wrapper to assign [v] to [value]
 void AssignValue(Value& v, bool value)
 {
     v.b = value;
 }
 
+/// wrapper to assign [v] to [value]
 void AssignValue(Value& v, double value)
 {
     v.d = value;
 }
 
+/// wrapper to assign [v] to [value]
 void AssignValue(Value& v, int value)
 {
     v.i = value;
--- src/value.h
+++ src/value.h
@@ -3,6 +3,12 @@
 
 #include "abstract.h"
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Value union
+
+/// a Value is a memory block used to store the value of Primitive calls which
+/// should be 64 bits in size and can refer to the following
 union Value
 {
     std::int64_t i;
@@ -11,9 +17,13 @@ union Value
     std::string* s;
 };
 
+/// wrapper to construct a new std::string used as a Call value
 String* StringConstructor(const String& value);
+
+/// wrapper which should be used to destroy a std::string used as a Call value
 void StringDestructor(String* s);
 
+/// wrappers to assign [v] to [value]
 void AssignValue(Value& v, const String& value);
 void AssignValue(Value& v, bool value);
 void AssignValue(Value& v, double value);
