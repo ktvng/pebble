f69081bd5c2cf00273df307f27057d7e2df21467
Kevin Tang <just.ktang@gmail.com>
2020-08-21 01:09:32 -0400

    added basic demo capabilities

---
+++ demo/demo.h
@@ -0,0 +1,6 @@
+#ifndef __DEMO_H
+#define __DEMO_H
+
+int RunDemo();
+
+#endif
---
+++ demo/display.h
@@ -0,0 +1,9 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "abstract.h"
+
+void PrintLineToConsole(const Operation* op, int level);
+void PrintProgramToConsole(const Program* p);
+
+#endif
